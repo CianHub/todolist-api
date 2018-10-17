de718c96e6b30dcfa483f6b50b0895d759808fe9
Cian O'Gralaigh <ihatecian@gmail.com>
2018-10-17 14:52:18 +0100

    clean up file structure, insure, install nodemon, create script to watch code and styles at once, add index.html

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ const express = require("express"),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Grants access to the views folder in the directory (__dirname)
+app.use(express.static(__dirname + "/views"));
+
+// Grants access to the public folder in the directory (__dirname)
+app.use(express.static(__dirname + "/public"));
+
 // app.get() creates a route, sends a responsie
-app.get("/", (req, res) => res.json({ name: "Cian" }));
+app.get("/", (req, res) => res.sendfile("index.html"));
 
 // The base url for the routes imported in the todoroutes variable
 app.use("/api/todos", todoRoutes);
